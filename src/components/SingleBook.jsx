142e6f453cef0d243b39e81725314d3efd19e92c
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-06-27 15:16:37 +0200

    aggiunto onclick sulla card con implento del toggle del bordo su di essa

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,19 +1,28 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
+import { Component } from "react";
 
-const SingleBook = (props) => {
-  //console.log(props.bookTitle.title);
-  return (
-    <Card>
-      <Card.Img variant="top" src={props.book.img} />
-      <Card.Body>
-        <Card.Title>{props.book.title}</Card.Title>
-        <Card.Text>Category: {props.book.category}</Card.Text>
-        <Badge bg="dark">€ {props.book.price}</Badge>
-        <Button variant="primary">Buy</Button>
-      </Card.Body>
-    </Card>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  render() {
+    return (
+      <Card
+        onClick={() => this.setState({ selected: !this.state.selected })}
+        style={{ border: this.state.selected ? "2px solid grey" : "none" }}
+      >
+        <Card.Img variant="top" src={this.props.book.img} />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>Category: {this.props.book.category}</Card.Text>
+          <Badge bg="dark">€ {this.props.book.price}</Badge>
+          <Button variant="primary">Buy</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 export default SingleBook;
